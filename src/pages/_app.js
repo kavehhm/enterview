4f628ae325b9d91120bc77213c43d0741e7b42b4
wuavonft <kavehm013@gmail.com>
2023-10-27 23:53:54 -0700

    ain

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+  <div className='p-10'>
+   <p className='font-bold'>
+      enter
+      <span className='text-blue-600'>view</span>
+     </p>
+  <Component {...pageProps} />
+  </div>)
 }
